0a352cd53304abf8af59a7ac8a664268db63fb6d
lasen7 <lasen7@naver.com>
2017-01-26 12:34:11 +0900

    Setting middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,49 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import session from 'express-session';
+
+import mongoose from 'mongoose';
+import connectMongo from 'connect-mongo';
 
 import api from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const MongoStore = connectMongo(session);
+
+/* SETUP MIDDLEWARE */
+
+app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 14 * 24 * 60 * 60 * 1000  // 14 days
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 14 * 24 * 60 * 60            // 14 days
+  })
+}));
+
+// setup morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // setup router
 app.use('/api', api);
+
+// Not found
+app.use((req, res, next) => {
+  res.status(404).send({ msg: 'Not found' });
+});
 
 // handle error
 app.use((err, req, res, next) => {
@@ -15,6 +52,16 @@ app.use((err, req, res, next) => {
     msg: err.message
   });
 });
+
+// Connect mongodb
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('Connected to mongod server');
+});
+mongoose.connect(process.env.DB_MONGO);
 
 app.listen(port, () => {
   console.log('Server start port on ', port);
