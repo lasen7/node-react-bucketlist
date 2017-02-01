03f4ecbe2d66946e3ce5302f975564398866c0c8
lasen7 <lasen7@naver.com>
2017-02-01 11:17:12 +0900

    Add review reported post API

--- server/src/controllers/report.js
+++ server/src/controllers/report.js
@@ -74,3 +74,59 @@ export const getReportedPosts = (req, res, next) => {
       next(err);
     });
 };
+
+export const reviewPost = (req, res, next) => {
+  const validateParams = validateObjectId(req.params.postId);
+  if (!validateParams) {
+    return res.status(400).send({
+      msg: 'Invalid postId',
+      code: 2
+    });
+  }
+
+  if (!req.user) {
+    return res.status(401).send({
+      msg: 'Not authorized',
+      code: 1
+    });
+  }
+
+  Account.findUser(req.user.common_profile.username)
+    .then(account => {
+      if (!account) {
+        let error = new Error();
+        error.message = 'Not found user';
+        error.code = 400;
+        error.errorCode = 3;
+        throw error;
+      }
+
+      if (!account.admin) {
+        let error = new Error();
+        error.message = 'Not authorized';
+        error.code = 401;
+        error.errorCode = 1;
+        throw error;
+      }
+
+      return Report.findReport(req.params.postId);
+    })
+    .then(report => {
+      if (!report) {
+        let error = new Error();
+        error.message = 'Not found report';
+        error.code = 400;
+        error.errorCode = 4;
+        throw error;
+      }
+
+      report.review = true;
+      report.save();
+    })
+    .then(() => {
+      res.send({ msg: 'SUCCESS' });
+    })
+    .catch(err => {
+      next(err);
+    });
+};
--- server/src/routes/report.js
+++ server/src/routes/report.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/post/:postId', report.reportPost);
 router.get('/post', report.getReportedPosts);
+router.post('/post/:postId/review', report.reviewPost);
 
 export default router;
