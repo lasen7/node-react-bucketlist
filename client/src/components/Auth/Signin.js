6351f3ab4917ce60d888441761f45e532f32d16c
lasen7 <lasen7@naver.com>
2017-02-03 23:11:47 +0900

    done Signin component design

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -3,25 +3,31 @@ import React from 'react';
 const Signin = () => {
   return (
     <div className="signin">
-      <div className="box">
+      <div className="box animated fadeInRight">
         <div className="ui form">
           <div className="field">
             <label>아이디</label>
-            <input placeholder="아이디" />
+            <div className="ui left icon input">
+              <i aria-hidden="true" className="user icon"></i>
+              <input placeholder="아이디" />
+            </div>
           </div>
 
           <div className="field">
             <label>비밀번호</label>
-            <input placeholder="비밀번호" />
+            <div className="ui left icon input">
+              <i aria-hidden="true" className="lock icon"></i>
+              <input placeholder="비밀번호" />
+            </div>
           </div>
 
           <div className="field">
             <button type="submit" className="ui fluid button">로그인</button>
           </div>
 
-          <div className="ui horizontal divider">
+          <div className="ui horizontal inverted divider">
             OR
-        </div>
+          </div>
 
           <div className="field">
             <button className="ui facebook fluid button">
