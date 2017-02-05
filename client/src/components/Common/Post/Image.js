fbc4c66863a2b6ab716e330cab76caa1fda6769d
lasen7 <lasen7@naver.com>
2017-02-05 11:08:40 +0900

    Add image section of Post component

---
+++ client/src/components/Common/Post/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = () => {
+  return (
+    <img className="ui fluid image" src="http://www.placehold.it/960x960" />
+  );
+};
+
+export default Image;
--- client/src/components/Common/Post/Post.js
+++ client/src/components/Common/Post/Post.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import UserInfo from './UserInfo';
+import Image from './Image';
 
 const Post = () => {
   return (
     <div className="post">
       <UserInfo />
+      <Image />
     </div>
   );
 };
