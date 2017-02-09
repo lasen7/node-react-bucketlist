1ab5d2fa0e6c087e4f51ae51902858a6b019a842
lasen7 <lasen7@naver.com>
2017-02-10 01:23:33 +0900

    Add react-dropzone module

--- client/src/components/Write/UploadImage.js
+++ client/src/components/Write/UploadImage.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import image from 'assets/sample-image.jpg';
+import Dropzone from 'react-dropzone';
 
-const UploadImage = () => {
-  const empty = (
-    <div className="empty-image">
-      <i className="image icon"></i>
-    </div>
-  );
+class UploadImage extends Component {
 
-  return (
-    <div className="upload-image">
-      <img className="ui flid image" src={image} alt="" />
-      {/*{empty}*/}
-    </div>
-  );
-};
+  state = {
+    preview: '',
+    filename: ''
+  }
+
+  onDrop = (files) => {
+    this.setState({
+      preview: files[0].preview,
+      filename: files[0].name
+    });
+  }
+
+  handleRemove = () => {
+    this.setState({
+      preview: '',
+      filename: ''
+    });
+  }
+
+  render() {
+    const {preview, filename} = this.state;
+
+    const image = (
+      <div className="preview">
+        <img className="ui flid image" src={preview} alt="" />
+        <i
+          className="circular teal remove inverted icon"
+          onClick={this.handleRemove}>
+        </i>
+      </div>
+    );
+
+    return (
+      <div className="upload-image">
+        {preview ? image :
+          <Dropzone className="dropzone" onDrop={this.onDrop} multiple={false}>
+            <div>사진을 올려주세요</div>
+          </Dropzone>
+        }
+      </div>
+    );
+  }
+}
 
 export default UploadImage;
